d32d1c12b7ec34dd18c768cb6b7f7e034f563001
Guanhua <william.ting.1116@gmail.com>
2019-01-24 11:34:33 +1100

    finished landing page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_23_013403) do
+ActiveRecord::Schema.define(version: 2019_01_23_232340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2019_01_23_013403) do
     t.integer "user_id"
     t.integer "vacancy"
     t.text "passengers", default: [], array: true
+    t.integer "price"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,122 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+User.create(
+  :fullname => 'Guanhua Ding',
+  :email => 'gd@ga.co',
+  :password => 'chicken',
+  :password_confirmation => 'chicken'
+)
+User.create(
+  :fullname => 'One General',
+  :email => 'one@ga.co',
+  :password => 'chicken',
+  :password_confirmation => 'chicken'
+)
+User.create(
+  :fullname => 'Two General',
+  :email => 'two@ga.co',
+  :password => 'chicken',
+  :password_confirmation => 'chicken'
+)
+User.create(
+  :fullname => 'Three General',
+  :email => 'three@ga.co',
+  :password => 'chicken',
+  :password_confirmation => 'chicken'
+)
+User.create(
+  :fullname => 'Four General',
+  :email => 'four@ga.co',
+  :password => 'chicken',
+  :password_confirmation => 'chicken'
+)
+
+user1 = User.find_by :email => 'gd@ga.co'
+user2 = User.find_by :email => 'one@ga.co'
+user3 = User.find_by :email => 'two@ga.co'
+user4 = User.find_by :email => 'three@ga.co'
+user5 = User.find_by :email => 'four@ga.co'
+
+Route.destroy_all
+Route.create(
+  :start => 'Gold Coast',
+  :end => 'Brisbane',
+  :capacity => '3',
+  :price => 10,
+  :user_id => user1.id
+)
+
+Route.create(
+  :start => 'Sydney',
+  :end => 'Melbourne',
+  :capacity => '3',
+  :price => 20,
+  :user_id => user1.id
+)
+
+Route.create(
+  :start => 'Logan',
+  :end => 'Byron Bay',
+  :capacity => '3',
+  :price => 15,
+  :user_id => user2.id
+)
+
+Route.create(
+  :start => 'Brisbane Airport',
+  :end => 'Brisbane CBD',
+  :capacity => '3',
+  :price => 10,
+  :user_id => user2.id
+)
+
+Route.create(
+  :start => 'Newcastle',
+  :end => 'Sydney',
+  :capacity => '3',
+  :price => 20,
+  :user_id => user3.id
+)
+
+Route.create(
+  :start => 'Sydney',
+  :end => 'Central Coast',
+  :capacity => '3',
+  :price => 15,
+  :user_id => user3.id
+)
+
+Route.create(
+  :start => 'Melbourne',
+  :end => 'Canberra',
+  :capacity => '3',
+  :price => 30,
+  :user_id => user4.id
+)
+
+Route.create(
+  :start => 'Adelaide',
+  :end => 'Melbourne',
+  :capacity => '3',
+  :price => 35,
+  :user_id => user4.id
+)
+
+Route.create(
+  :start => 'Cairns',
+  :end => 'Brisbane',
+  :capacity => '3',
+  :price => 50,
+  :user_id => user5.id
+)
+
+Route.create(
+  :start => 'Perth',
+  :end => 'Darwin',
+  :capacity => '3',
+  :price => 100,
+  :user_id => user5.id
+)
